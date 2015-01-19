09473cbb3fa6d9bb5f17d010efab37c6237bb102
Zack Cooper <zackjcooper@gmail.com>
2015-01-19 14:31:20 -0500

    made the tests pep8 approved

--- test_get_random_word.py
+++ test_get_random_word.py
@@ -4,19 +4,24 @@ from mystery_word import get_random_word
 def test_does_word_return():
     assert type(get_random_word(1)) == str
 
+
 def test_difficulty_easy():
     word_length = len(get_random_word(1))
     assert word_length >= 4 and word_length <= 6
+
 
 def test_difficulty_medium():
     word_length = len(get_random_word(2))
     assert word_length >= 6 and word_length <= 10
 
+
 def test_difficulty_hard():
     assert len(get_random_word(3)) >= 10
 
+
 def test_other_difficulty():
     assert len(get_random_word('hi')) > 15
+
 
 def test_no_value_return():
     assert len(get_random_word(89)) > 1
--- test_user_process_w_answer.py
+++ test_user_process_w_answer.py
@@ -3,15 +3,17 @@ from mystery_word import guess_in_answer
 from mystery_word import incorrect_guess
 
 
-
 def test_correct_guess_not_win():
     assert guess_in_answer('hola', ['o'], 7) == ['o']
+
 
 def test_bad_guess_handling():
     assert incorrect_guess(5, '5', 'hello') == 5
 
+
 def test_good_letter_guess():
     assert incorrect_guess(3, 'r', 'hola') == 2
+
 
 def test_symbols_should_be_removed():
     assert incorrect_guess(4, '%', 'hola') == 4
