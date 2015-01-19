26dfa73d8f65cc3238263b7ed5dd1673fe138b1b
Zack Cooper <zackjcooper@gmail.com>
2015-01-19 11:41:08 -0500

    final cleanup

--- test_guess_in_answer.py
+++
@@ -1,5 +0,0 @@
-from mystery_word import guess_in_answer
-
-
-def correct_guess_not_win():
-    assert guess_in_answer('hola', ['o'], 7) == ['o']
--- test_user_guess_process.py
+++
@@ -1,5 +0,0 @@
-from mystery_word import user_guess_process
-
-
-def full_user_run():
-    assert user_guess_process('hola') == True
