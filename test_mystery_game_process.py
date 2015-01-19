5b6e3abce8ff323e3802f030a1f1009665f510e4
Zack Cooper <zackjcooper@gmail.com>
2015-01-19 11:00:19 -0500

    fixed another test

--- test_mystery_game_process.py
+++
@@ -1,8 +0,0 @@
-from mystery_word import mystery_game_process
-
-
-def successful_replay_game():
-    assert myster_game_process(playing_again=True) == True
-
-def successful_replay_game():
-    assert myster_game_process(playing_again=False) == True
