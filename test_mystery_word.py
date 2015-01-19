ca2020a711abb062234a6e4b0f064e0698f27a4a
Zack Cooper <zackjcooper@gmail.com>
2015-01-19 11:03:41 -0500

    fixed test_myster_word.py to contain the fixed tests

--- test_mystery_word.py
+++ test_mystery_word.py
@@ -5,10 +5,18 @@ def test_does_word_return():
     assert type(get_random_word(1)) == str
 
 def test_difficulty_easy():
-    assert len(get_random_word(1)) < 5
+    word_length = len(get_random_word(1))
+    assert word_length >= 4 and word_length <= 6
 
 def test_difficulty_medium():
-    assert len(get_random_word(1)) < 5
+    word_length = len(get_random_word(2))
+    assert word_length >= 6 and word_length <= 10
 
 def test_difficulty_hard():
-    assert len(get_random_word(1)) < 5
+    assert len(get_random_word(3)) >= 10
+
+def test_other_difficulty():
+    assert len(get_random_word('hi')) > 15
+
+def test_no_value_return():
+    assert len(get_random_word(89)) > 1
